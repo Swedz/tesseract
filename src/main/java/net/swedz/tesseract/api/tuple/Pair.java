d7e1ddb595a407aac61b09f2269a9d9d39c22644
Swedz <me@swedz.net>
2026-05-23 22:42:33 -0400

    Pair and Triple data objects

---
+++ src/main/java/net/swedz/tesseract/api/tuple/Pair.java
@@ -0,0 +1,5 @@
+package net.swedz.tesseract.api.tuple;
+
+public record Pair<A, B>(A a, B b)
+{
+}
---
+++ src/main/java/net/swedz/tesseract/api/tuple/Triple.java
@@ -0,0 +1,5 @@
+package net.swedz.tesseract.api.tuple;
+
+public record Triple<A, B, C>(A a, B b, C c)
+{
+}
